d6611f6e46f5636738b2efe7e2d0d0f9184f595d
Annisa A <aaanr0811@gmail.com>
2024-06-14 23:56:37 +0700

    employees(feat)fixing sidebar adding logout button

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -2,15 +2,31 @@
   <nav class="sidebar">
     <ul>
       <li>
-        <router-link to="/" active-class="active-link">Home</router-link>
+        <router-link to="/" active-class="active-link">
+          <i class="bi bi-house-door"></i> Home
+        </router-link>
       </li>
       <li>
-        <router-link to="/leave" active-class="active-link">Leave</router-link>
+        <router-link to="/leave" active-class="active-link">
+          <i class="bi bi-calendar"></i> Leave
+        </router-link>
       </li>
       <li>
-        <router-link to="/payroll" active-class="active-link">Payroll</router-link>
+        <router-link to="/payroll" active-class="active-link">
+          <i class="bi bi-cash-stack"></i> Payroll
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/profile" active-class="active-link">
+          <i class="bi bi-person"></i> Profile
+        </router-link>
       </li>
     </ul>
+    <div class="logout">
+      <router-link to="/login" active-class="active-link">
+        <i class="bi bi-box-arrow-right"></i> Logout
+      </router-link>
+    </div>
   </nav>
 </template>
 
